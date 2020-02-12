d4541d48e533ce830470d9ee58406011e5b72913
Jonathan Zacharuk <jonathan.zacharuk@ascentech.ca>
2020-02-12 14:43:56 -0800

    Updates

---
+++ precache-manifest.30320ceca7531072a850424dcc565548.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dfe027e9acb6c82e097009a55d8413e",
+    "url": "/adwiz/index.html"
+  },
+  {
+    "revision": "ca6cf2bfb87c0b6ceee5",
+    "url": "/adwiz/static/css/2.9ddb28ea.chunk.css"
+  },
+  {
+    "revision": "edc9ee968ef5358b399d",
+    "url": "/adwiz/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ca6cf2bfb87c0b6ceee5",
+    "url": "/adwiz/static/js/2.4bb02bec.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/adwiz/static/js/2.4bb02bec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edc9ee968ef5358b399d",
+    "url": "/adwiz/static/js/main.6486c7b1.chunk.js"
+  },
+  {
+    "revision": "cd037f4048628556f128",
+    "url": "/adwiz/static/js/runtime-main.80ce7624.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/adwiz/static/media/logo.5d5d9eef.svg"
+  }
+]);
